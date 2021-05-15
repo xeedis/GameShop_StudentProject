9d718719ed02f98de3daefc38f134caddd91a24c
Andrzej Mróz <andrzejmroz1999@gmail.com>
2021-05-15 19:16:30 +0200

    adding a cart page

---
+++ Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameShop.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
