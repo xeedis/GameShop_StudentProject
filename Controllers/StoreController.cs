c5e9c7b0926566ba5fc72c4990538180769ec421
Andrzej Mróz <andrzejmroz1999@gmail.com>
2021-05-15 19:10:53 +0200

    Adding a store page and StoreController

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using GameShop.DAL;
+using GameShop.Models;
+using GameShop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameShop.Controllers
+{
+    public class StoreController : Controller
+    {
+        private GameContext db = new GameContext();
+        // GET: Store
+        public ActionResult Store()
+        {
+            List<Game> allGames = db.Games.Where(a => !a.Hidden).OrderByDescending(a => a.Bestseller).ToList();
+
+            var vm = new StoreViewModel()
+            {
+                AllGames = allGames
+            };
+            return View(vm);
+            
+        }
+    }
+}
